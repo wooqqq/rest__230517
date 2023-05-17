20711a11122a31b78a6bb00eea45b90ee2ff96ab
깃wooqqq <sge0225@naver.com>
2023-05-17 15:33:35 +0900

    feat : JPA 도입 및 NotProd 이용하여 앱 시작시 회원 2명 자동 생성

--- src/main/java/com/ll/rest_api/base/security/SecurityConfig.java
+++ src/main/java/com/ll/rest_api/base/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -23,5 +25,10 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS)); // 세션 끄기
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/ll/rest_api/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/rest_api/boundedContext/member/controller/MemberController.java
@@ -1,14 +1,34 @@
 package com.ll.rest_api.boundedContext.member.controller;
 
+import com.ll.rest_api.boundedContext.member.entity.Member;
+import com.ll.rest_api.boundedContext.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/member")
+@RequestMapping(value = "/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 public class MemberController {
+    private final MemberService memberService;
+
+    @Data
+    public static class LoginRequest {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
     @PostMapping("/login")
-    public String login() {
-        return "성공";
+    public Member login(@Valid @RequestBody LoginRequest loginRequest) {
+        return memberService.findByUsername(loginRequest.getUsername()).orElse(null);
     }
 }
