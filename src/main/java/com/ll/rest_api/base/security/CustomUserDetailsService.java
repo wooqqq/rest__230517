9483b769fbf93b80c629208d6752fa30b44b5fcf
깃wooqqq <sge0225@naver.com>
2023-05-18 10:43:25 +0900

    feat : CustomUserDetailsService 생성 및 테스트케이스 성공

---
+++ src/main/java/com/ll/rest_api/base/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.ll.rest_api.base.security;
+
+import com.ll.rest_api.boundedContext.member.entity.Member;
+import com.ll.rest_api.boundedContext.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new User(member.getUsername(), member.getPassword(), member.getAuthorities());
+    }
+}
