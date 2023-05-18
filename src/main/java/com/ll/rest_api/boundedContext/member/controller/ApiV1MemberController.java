1b8b94deee3f282e90c4ffedd6a49e887bc01cdf
깃wooqqq <sge0225@naver.com>
2023-05-18 10:31:59 +0900

    feat : member/me 생성

--- src/main/java/com/ll/rest_api/boundedContext/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/rest_api/boundedContext/member/controller/ApiV1MemberController.java
@@ -1,6 +1,7 @@
 package com.ll.rest_api.boundedContext.member.controller;
 
 import com.ll.rest_api.base.rsData.RsData;
+import com.ll.rest_api.boundedContext.member.entity.Member;
 import com.ll.rest_api.boundedContext.member.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -9,11 +10,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.MediaType.ALL_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequiredArgsConstructor
@@ -37,13 +36,31 @@ public class ApiV1MemberController {
     }
 
     @PostMapping("/login")
-    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
+    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
 
         return RsData.of(
                 "S-1",
                 "엑세스 토큰이 생성되었습니다.",
                 new LoginResponse(accessToken)
+        );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class MeResponse {
+        private final Member member;
+    }
+
+    // consumes = ALL_VALUE => 나는 딱히 JSON 을 입력받기를 고집하지 않겠다
+    @GetMapping(value = "/me", consumes = ALL_VALUE)
+    public RsData<MeResponse> me() {
+        Member member = memberService.findByUsername("user1").get();
+
+        return RsData.of(
+                "S-1",
+                "성공",
+                new MeResponse(member)
         );
     }
 }
